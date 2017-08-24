2aaff5f668150c16d746aca5f008868806dcde40
koshelevandrey <Tikr22@gmail.com>
2017-08-24 20:04:18 +0300

    Вывод с помощью текстового поля и кнопок

--- getOutput.js
+++ getOutput.js
@@ -3,7 +3,7 @@
 function getOutputString(array) { // Функция, возвращающая строку для вывода (array - ассоциативный массив)
     var returnString='';
     for (var i in array){
-        returnString+=i+' = '+array[i]+'<br>';
+        returnString+=i+' = '+array[i]+'\n';
     }
     return returnString;
 }
@@ -11,17 +11,27 @@ function getOutputString(array) { // Функция, возвращающая с
 function getOutputStringForFuncs(array) { // Для функций
     var returnString='';
     for (var i in array){
-        returnString+=i+'<br>';
+        returnString+=i+'\n';
     }
     return returnString;
 }
 
-var constants = {'x': '15', 'y': '7', 'len': '45289485029845000000000000000000000000000000000000'}; // Создаём ассоциативный массив
-constants['N'] = 100; // Добавим ещё элемент
-document.getElementById("constantsTab").innerHTML=getOutputString(constants);
-
 var userVariables = {'a': 20, 'min': 53, 'max': 67, 'k':121231241351235, 'name':'Petya'};
-document.getElementById("variablesTab").innerHTML=getOutputString(userVariables);
+var outputString1=getOutputString(userVariables);
 
 var functions = { 'fib':'', 'findMax':'', 'reverse':''};
-document.getElementById("functionsTab").innerHTML=getOutputStringForFuncs(functions);
+var outputString2=getOutputStringForFuncs(functions);
+
+document.getElementById('output').value= outputString1;
+document.getElementById('variablesButton').style.backgroundColor = '#778899';
+
+document.getElementById('variablesButton').onclick = function() {
+    document.getElementById('output').value= outputString1;
+    document.getElementById('variablesButton').style.backgroundColor = '#778899';
+    document.getElementById('functionsButton').style.backgroundColor = '#FFFFFF';
+};
+document.getElementById('functionsButton').onclick = function() {
+    document.getElementById('output').value= outputString2;
+    document.getElementById('variablesButton').style.backgroundColor = '#FFFFFF';
+    document.getElementById('functionsButton').style.backgroundColor = '#778899';
+};
