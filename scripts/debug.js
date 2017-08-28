0cb5c043f506745929cb64ac978a0d19dec543aa
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2017-08-28 20:41:18 +0300

    Update debug.js

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,4 +1,4 @@
 function show(state) {
-	document.getElementById('window').style.display = state;			
-	document.getElementById('wrap').style.display = state; 			
+  document.getElementById('window').style.display = state;      
+  document.getElementById('wrap').style.display = state;      
 }
