36a97b2f3de7f4d0e3b68eac6ed049e4815179c0
Gevorg <tsarukyangt98@mail.ru>
2017-08-31 12:51:16 +0300

    Update interpret_release.js
    Файл изменен таким образом, чтобы словари были всегда обновлены.

--- scripts/interpret_release.js
+++ scripts/interpret_release.js
@@ -8,6 +8,31 @@ function isSpace(n) {
   return n == '\n' || n == ' ' || n == '\t' || n == '\r';
 }
 
+var maxTextLen1=14; // Максимальная длина строки для констант
+var maxTextLen2=21; // Максимальная длина строки для базовых функций
+
+function getOutputString(array) { // Функция, возвращающая строку для вывода (здесь array - ассоциативный массив)
+    var returnString='';
+    var len;
+    for (var i in array){
+        returnString+=i+' = '+array[i]+'\n';
+        len=i.length+3+String(array[i]).length;
+        if (len>maxTextLen1) maxTextLen1=len;
+    }
+    return returnString;
+}
+
+function getOutputStringForFuncs(array) { // Получение строки вывода для функций
+    var returnString='';
+    var len;
+    for (var i in array){
+        returnString+=i+'\n';
+        len=i.length;
+        if (len>maxTextLen2) maxTextLen2=len;
+    }
+    return returnString;
+}
+
 function findEnd( i, t ) {
   var k = 1;
   while ( k != 0 ) {
@@ -71,6 +96,29 @@ math.Sqrt2 = 1.414;
 math.Sqrt3 = 1.732;
 var values = {};
 var dictionary = {};
+
+var variablesString=getOutputString(values);
+var outputString1='Константы:\n' + '\n' +
+    'E = 2.718\n' + 'Tau = 6.283\n' + 'Pi = 3.142\n' +
+    'Log2E = 1.443\n' + 'Log10E = 0.434\n' + 'Ln2 = 0.693\n' +
+    'Ln10 = 2.302\n' + 'Sqrt2 = 1.414\n' + 'Sqrt3 = 1.732\n';
+if (maxTextLen1<dic_func_output.cols) maxTextLen1=dic_func_output.cols;
+for (var i=maxTextLen1;i>0;i--) {
+    outputString1+='_';
+}
+outputString1+='\n'+'Переменные:\n'+'\n';
+
+var functionsString=getOutputStringForFuncs(dictionary);
+var outputString2='Базовые функции:\n' + '\n' + 'define..end\n' + 'exit\n' +
+    'if..endif\n' + 'while..endwhile\n' + 'var\n' + 'drop\n' + 'drop-all\n' +
+    'swap\n' + 'dup\n' + 'over\n' + 'depth\n' + 'rot\n' + 'and\n' + 'or\n' +
+    'not\n' + '=\n' + '>\n' + '<\n' + '+\n' + '-\n' + '*\n' +
+    '/\n' + 'div\n' + 'mod\n' + 'neg\n';
+if (maxTextLen2<dic_func_output.cols) maxTextLen2=dic_func_output.cols;
+for (var i=maxTextLen2;i>0;i--) {
+    outputString2+='_';
+}
+outputString2+='\n'+'Функции пользователя:\n'+'\n';
 
 document.getElementById('runButton').onclick = (event) => {
   var text = editor.getValue();
@@ -324,62 +372,14 @@ function decoding( tokens, index, stack, returnStack, dictionary ) {
   return stack;
 };
 ///////////////////далее функции для вывода словарей
-var maxTextLen1=14; // Максимальная длина строки для констант
-var maxTextLen2=21; // Максимальная длина строки для базовых функций
 
-function getOutputString(array) { // Функция, возвращающая строку для вывода (здесь array - ассоциативный массив)
-    var returnString='';
-    var len;
-    for (var i in array){
-        returnString+=i+' = '+array[i]+'\n';
-        len=i.length+3+String(array[i]).length;
-        if (len>maxTextLen1) maxTextLen1=len;
-    }
-    return returnString;
-}
-
-function getOutputStringForFuncs(array) { // Получение строки вывода для функций
-    var returnString='';
-    var len;
-    for (var i in array){
-        returnString+=i+'\n';
-        len=i.length;
-        if (len>maxTextLen2) maxTextLen2=len;
-    }
-    return returnString;
-}
-
-var variablesString=getOutputString(values);
-var outputString1='Константы:\n' + '\n' +
-    'E = 2.718\n' + 'Tau = 6.283\n' + 'Pi = 3.142\n' +
-    'Log2E = 1.443\n' + 'Log10E = 0.434\n' + 'Ln2 = 0.693\n' +
-    'Ln10 = 2.302\n' + 'Sqrt2 = 1.414\n' + 'Sqrt3 = 1.732\n';
-if (maxTextLen1<dic_func_output.cols) maxTextLen1=dic_func_output.cols;
-for (var i=maxTextLen1;i>0;i--) {
-    outputString1+='_';
-}
-outputString1+='\n'+'Переменные:\n'+'\n';
-// Конкатенируем outputString1 со списком переменных
-outputString1+=variablesString;
-
-var functionsString=getOutputStringForFuncs(dictionary);
-var outputString2='Базовые функции:\n' + '\n' + 'define..end\n' + 'exit\n' +
-    'if..endif\n' + 'while..endwhile\n' + 'var\n' + 'drop\n' + 'drop-all\n' +
-    'swap\n' + 'dup\n' + 'over\n' + 'depth\n' + 'rot\n' + 'and\n' + 'or\n' +
-    'not\n' + '=\n' + '>\n' + '<\n' + '+\n' + '-\n' + '*\n' +
-    '/\n' + 'div\n' + 'mod\n' + 'neg\n';
-if (maxTextLen2<dic_func_output.cols) maxTextLen2=dic_func_output.cols;
-for (var i=maxTextLen2;i>0;i--) {
-    outputString2+='_';
-}
-outputString2+='\n'+'Функции пользователя:\n'+'\n';
-outputString2+=functionsString;
-
-document.getElementById('dic_func_output').value= outputString1;
+document.getElementById('dic_func_output').value= outputString1 + variablesString;
 
 document.getElementById('variablesButton').onclick = function() {
-    document.getElementById('dic_func_output').value= outputString1;
+  variablesString=getOutputString(values);
+  document.getElementById('dic_func_output').value= outputString1 + variablesString;
 };
 document.getElementById('functionsButton').onclick = function() {
-    document.getElementById('dic_func_output').value= outputString2;
+  functionsString=getOutputStringForFuncs(dictionary);
+  document.getElementById('dic_func_output').value= outputString2 + functionsString;
 };
