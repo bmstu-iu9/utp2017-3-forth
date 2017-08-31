4779147e57e2f8db422f1bc262948f96a385fc83
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2017-08-31 14:58:19 +0300

    Update debug.js

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,4 +1,11 @@
 function show(state) {
-  document.getElementById('window').style.display = state;      
-  document.getElementById('wrap').style.display = state;      
+	document.getElementById('window').style.display = state;      
+ 	document.getElementById('wrap').style.display = state;
+ 	if (state == 'none') {
+ 		inputD.setValue();
+ 		var bp = document.getElementById("inputD").getElementsByClassName("break_point")[0];
+ 		if(bp) {
+ 			bp.remove();
+ 		}
+ 	}
 }
