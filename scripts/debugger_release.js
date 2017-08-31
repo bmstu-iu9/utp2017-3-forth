2bc8795915a502596c1cd155287cd76d0aa19fb0
Gevorg <tsarukyangt98@mail.ru>
2017-08-31 15:05:00 +0300

    Add files via upload
    Исправлено отсутствие var (непонятно, куда пропала часть кода)

--- scripts/debugger_release.js
+++ scripts/debugger_release.js
@@ -715,6 +715,14 @@ document.getElementById('last1').onclick = (event) => {
                     index = returnStack.pop();
                     nos = numbers[index];
                     break;
+                  case 'var':
+                    if ( isNumber(tokens[index + 2] ) ) {
+                      values[ tokens[index+1] ] = Number( tokens[index+2] );
+                    } else {
+                      error = true;
+                    }
+                    index = index + 2;
+                    break;
                   default:
                     if (str in dictionary) {
                       returnStack.push(index);
